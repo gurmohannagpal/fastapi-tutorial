92b5b3218cfb81782b23b16666e856697b0762c0
gurmohannagpal <51723607+gurmohannagpal@users.noreply.github.com>
2022-02-13 09:52:20 -0800

    fixed database_hostaname to database_hostname in config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostaname: str
+    database_hostname: str
     database_port: str
     database_password: str 
     database_name: str
